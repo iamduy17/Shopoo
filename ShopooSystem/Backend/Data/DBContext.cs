796fddac307882aca91abb2714826db4d3915092
iamduy17 <thaiduydo17@gmail.com>
2024-03-27 13:23:22 +0700

    Set up project for Backend, Install package related like Entity Framework Core

---
+++ ShopooSystem/Backend/Data/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.DBContext
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+       
+    }
+}
